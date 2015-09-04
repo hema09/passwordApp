cf24568e5bb74bfdaaf2b02aa12b753e32b173a9
unknown <hbhatia@USSEAAWN909ZAFA.apollogrp.edu>
2015-09-04 16:41:44 -0700

    update

--- js/account.js
+++ js/account.js
@@ -27,7 +27,7 @@ app.controller("loginController", ['$scope', '$location', function($scope, $loca
 	}
 }]);
 
-app.controller("questionController", ['$scope', '$location', function($scope, $location) {
+/*app.controller("questionController", ['$scope', '$location', function($scope, $location) {
 	// GET these questions
 			$scope.questions = [
 				{"name":"What is blue?", "id" : "q1", "selected" : false},
@@ -36,7 +36,7 @@ app.controller("questionController", ['$scope', '$location', function($scope, $l
 				{"name":"What is an orange?", "id" : "q4","selected" : false},
 				{"name":"What is a duck?", "id" : "q5","selected" : false}
 			];	
-			$scope.userQuestions = [];
+			$scope.userQuestions = [];			
 			$scope.qnums = ["ques1", "ques2", "ques3"];
 			$scope.setQuestion = {};
 			
@@ -59,4 +59,63 @@ app.controller("questionController", ['$scope', '$location', function($scope, $l
 				// POST selected questions and answers
 				$location.path('/thankyou');
 			};
+
+			$scope.updateSelected = function(index) {
+				console.log("index=" + index);
+				console.log("selected = " + $scope.questions[index].selected)
+				$scope.questions[index].selected = !$scope.questions[index].selected;
+				console.log("selected = " + $scope.questions[index].selected)
+			};
 }]);
+*/
+
+app.controller("questionController", ['$scope', '$location', function($scope, $location) {
+	// GET these questions
+			$scope.questions = [
+				{"name":"What is blue?", "id" : "q1", "selected" : false},
+				{"name":"What is your phone's color?", "id" : "q2","selected" : false},
+				{"name":"Chicken is called?", "id" : "q3","selected" : false},
+				{"name":"What is an orange?", "id" : "q4","selected" : false},
+				{"name":"What is a duck?", "id" : "q5","selected" : false}
+			];	
+			$scope.userQuestions = [];	
+			$scope.totalrequired = 3;		
+			$scope.qnums = ["ques1", "ques2", "ques3"];
+			$scope.setQuestion = [];
+			
+			// function to add user with questions
+			$scope.addUserQuestions = function() {
+				console.log($scope.setQuestion);
+				$scope.setQuestion.map(function(ques){					
+					$scope.userQuestions.push({
+						question : ques.question.id,
+						answer : ques.answer
+					});
+				});
+				console.log($scope.userQuestions);
+				// POST selected questions and answers
+				$location.path('/thankyou');
+			};
+
+			$scope.updateSelected = function(question) {
+				console.log(question);
+				console.log("selected = " + question.selected)
+				question.selected = !question.selected;
+				console.log("selected = " + question.selected)
+			};
+}]);
+
+app.filter('excludeUsed', function() {
+	var filter = function(questionList) {
+		var checkItem = function(item) {
+			var use = true;
+			questionList.map(function(ques) {
+				if(ques.selected===true)
+					use = false;
+			});
+			return use;
+		};
+		return questionList.filter(checkItem);
+	};
+	return filter;
+});
